0a7c113c0ab171e4165745b740737e10e6bc4aea
ImtiarRafi <imtiarrafi@gmail.com>
2024-04-26 21:43:40 +0600

    Update code with new changes

--- number guess.py
+++ number guess.py
@@ -1,74 +1,83 @@
 import random
 
-my_number = int(input("Enter the number : "))
+print('----------- Lets Begin -------------')
+print(' ----Can you guess the number? -----')
+print('Remember you only have 3 attempts')
+
+
+my_number = int(input("Enter the number to guess : "))
 
 attempt = 0
+low = int(input('Enter your lowest point in the range : '))
+high = int(input('Enter your highest point in the range : '))
 
 while attempt < 3:
-    guess1 = random.randint(1, 10)
+    guess1 = random.randint(low, high)
     if guess1 > my_number:
-        print('Guess', {guess1}, ' is High')
+        print(f"{guess1} is High.\nHint: Guess Low")
         attempt += 1
 
-        guess2 = random.randint(1,guess1)
+        guess2 = random.randint(low,guess1)
         if guess2 > my_number:
-            print('Guess', {guess2}, ' is High')
+            print(f"{guess2} is High.\nHint: Guess Low")
             attempt += 1
 
-            guess3 = random.randint(1,guess2)
+            guess3 = random.randint(low,guess2)
             if guess3 > my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
 
             elif guess3 < my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
 
         elif guess2 < my_number:
-            print('Guess', {guess2}, ' is low')
+            print(f"{guess2} is Low.\nHint: Guess High")
             attempt += 1
 
             guess3 = random.randint(guess2,guess1)
 
             if guess3 > my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
 
             elif guess3 < my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
+
     elif guess1 < my_number:
-        print('Guess',{guess1},'is low')
+        print(f"{guess1} is Low.\nHint: Guess High")
         attempt += 1
 
-        guess2 = random.randint(guess1,10)
+        guess2 = random.randint(guess1,high)
 
         if guess2 > my_number:
-            print('Guess', {guess2}, ' is High')
+            print(f"{guess2} is High.\nHint: Guess Low")
             attempt += 1
 
             guess3 = random.randint(guess1,guess2)
             if guess3 > my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
 
             elif guess3 < my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
 
         elif guess2 < my_number:
-            print('Guess', {guess2}, ' is low')
+            print(f"{guess2} is Low.\nHint: Guess High")
             attempt += 1
 
-            guess3 = random.randint(guess2,10)
+            guess3 = random.randint(guess2,high)
 
             if guess3 > my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
 
             elif guess3 < my_number:
-                print('No Guess Left')
+                print(f'{guess3} is wrong.\n Unfortunately No Guesses Left')
                 attempt += 1
 
     else:
         print('Guess is Right')
+        break
